036a496b08bc8b5d14f8a4dd126e908266652945
Julia Pollack <julia@benchprep.com>
2021-07-02 16:02:30 -0500

    Validate payment amount

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,12 @@
 class Payment < ActiveRecord::Base
   belongs_to :loan
+
+  validates :loan, presence: true
+  validate :for_outstanding_balance
+
+  def for_outstanding_balance
+    unless amount > 0 && amount <= loan.outstanding_balance
+      errors.add(:amount, "must be greater than 0 and less than or equal to the loan's outstanding balance")
+    end
+  end
 end
