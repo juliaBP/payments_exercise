b8f5172c33c40b47dd8b438185368c688a6d6144
Julia Pollack <julia@benchprep.com>
2021-07-02 16:49:06 -0500

    Validate funded amount and oustanding balance on loan

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,3 +1,24 @@
 class Loan < ActiveRecord::Base
   has_many :payments
+
+  after_initialize :set_outstanding_balance
+  validate :funded_amount_ok?
+  validate :outstanding_balance_ok?
+
+  def set_outstanding_balance
+    return unless new_record?
+    self.outstanding_balance = funded_amount
+  end
+
+  def funded_amount_ok?
+    unless funded_amount > 0
+      errors.add(:funded_amount, "must be more than 0")
+    end
+  end
+
+  def outstanding_balance_ok?
+    unless outstanding_balance >= 0 && outstanding_balance <= funded_amount
+      errors.add(:outstanding_balance, "must be inclusively between 0 and the funded amount")
+    end
+  end
 end
